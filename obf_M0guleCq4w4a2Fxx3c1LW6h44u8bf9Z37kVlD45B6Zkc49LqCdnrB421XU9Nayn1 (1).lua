9fea0dca50d9e39eff4f5cc0cecd0c2d2b334642
khoaptst120 <157712831+khoaptst120@users.noreply.github.com>
2024-01-25 20:37:27 +0700

    Add files via upload

---
+++ obf_M0guleCq4w4a2Fxx3c1LW6h44u8bf9Z37kVlD45B6Zkc49LqCdnrB421XU9Nayn1 (1).lua
@@ -0,0 +1,3 @@
+_G.Name = "lacgay" -- Put You Name Script Here
+_G.Logo = "16108472997" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
